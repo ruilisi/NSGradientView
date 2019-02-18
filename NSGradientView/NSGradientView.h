e83c642179639920b9619556684099cc47bcb058
Jiawei Li <c@rallets.com>
2019-02-19 00:18:18 +0800

    Initial Commit

---
+++ NSGradientView/NSGradientView.h
@@ -0,0 +1,19 @@
+//
+//  NSGradientView.h
+//  NSGradientView
+//
+//  Created by Jiawei Li on 19/02/2019.
+//  Copyright © 2019 Paiyou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSGradientView.
+FOUNDATION_EXPORT double NSGradientViewVersionNumber;
+
+//! Project version string for NSGradientView.
+FOUNDATION_EXPORT const unsigned char NSGradientViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSGradientView/PublicHeader.h>
+
+
